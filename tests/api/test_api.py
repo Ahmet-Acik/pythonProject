63e1657aaf3ea4e9d81257b950a8cce85aec90e1
Ahmet Acik <a.acik@icloud.com>
2024-10-03 14:00:53 +0100

    Added webdriver_manager to requirements.txt file:

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,5 +1,7 @@
 import requests
+from tenacity import retry, stop_after_attempt, wait_fixed
 
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
 def test_get_request():
     response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
     assert response.status_code == 200
